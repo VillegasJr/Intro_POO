c808414cb1ec2492351e4ded698a3155e82541a5
VILLEGAS <josuetroya13@gmail.com>
2026-09-15 12:42:10 -0700

    Agregar archivos de proyecto.

---
+++ Intro_POO/Program.cs
@@ -0,0 +1,52 @@
+﻿using Intro_POO.clases;
+using Humanizer;
+
+/*
+Persona persona = new Persona(); // Create an instance of the Persona class
+persona.Nombre = "Raul"; // Set the Nombre property"
+persona.ApellidoP = "Gonzalez"; // Set the ApellidoP property
+persona.ApellidoM = "Lopez"; // Set the ApellidoM property
+persona.Edad = 30; // Set the Edad property
+
+// Este es un ejemplo de referencia, ya que personaDos apunta a la misma dirección de memoria que persona (Pointer).
+Persona personaDos = persona; // Assign the reference of persona to personaDos
+persona.Nombre = "Julia"; // Set the Nombre property"
+persona.ApellidoP = "Garcia"; // Set the ApellidoP property
+persona.ApellidoM = "Orlando"; // Set the ApellidoM property
+persona.Edad = 26; // Set the Edad property
+
+// De esta manera, personaDos también refleja los cambios realizados en persona, ya que ambos apuntan a la misma instancia de la clase Persona.
+Persona persona2 = new Persona();
+persona.Nombre = "Julia"; // Set the Nombre property"
+persona.ApellidoP = "Garcia"; // Set the ApellidoP property
+persona.ApellidoM = "Orlando"; // Set the ApellidoM property
+persona.Edad = 26; // Set the Edad property
+
+Console.WriteLine($"Hola {persona.Nombre} {persona.ApellidoP} {persona.ApellidoM}" + 
+    $"tines la edad de {persona.Edad}");
+Console.WriteLine($"Hola {personaDos.Nombre} {personaDos.ApellidoP} {personaDos.ApellidoM}" +
+    $"tines la edad de {personaDos.Edad}");
+*/
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+Persona personaTres = new Persona("Wendy", "Esquivel", "Calvario", 22); // Aqui se asignan los valores tal cual como los parametros del constructor de la clase Persona, en el orden que se definieron en el constructor.
+Persona persona3 = new Persona(lastname1: "Esquivel", lastname2: "Calvario", name: "Wendy", age: 22); // Aqui se asignan los valores de los parametros del constructor de la clase Persona, pero se especifica el nombre del parametro al que se le asigna el valor, por lo que no importa el orden en el que se definieron los parametros en el constructor.
+*/
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+DateTime nacimiento = Convert.ToDateTime("25/09/1995"); // Se crea una variable de tipo DateTime y se le asigna un valor de fecha, en este caso, la fecha de nacimiento de la persona.
+DateTime hoy = DateTime.Today;
+
+// Calcula la edad en años
+int edad = (hoy - nacimiento).Days / 365;
+
+// También puedes humanizar intervalos
+Console.WriteLine($"Edad: {edad} años");
+Console.WriteLine($"Tiempo transcurrido: {(hoy - nacimiento).Humanize()}");
+
+Persona personaCuatro = new Persona("Wendy", "Esquivel", "Calvario", nacimiento); // Aqui se asignan los valores tal cual como los parametros del constructor de la clase Persona, en el orden que se definieron en el constructor.
+Console.WriteLine($"Hola {personaCuatro.Nombre} {personaCuatro.ApellidoP} {personaCuatro.ApellidoM}" +
+    $"tienes la edad de {edad}");
---
+++ Intro_POO/clases/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Intro_POO.clases
+{
+    internal class Persona
+    {
+        // Contructor de la clase Persona, se ejecuta cuando se crea una instancia de la clase.
+        //public Persona(string name, string lastname1, string lastname2, int age, DateTime birthDate)
+            public Persona(string name, string lastname1, string lastname2, DateTime birthDate)
+        {
+            Nombre = name; // Asigna el valor del parámetro name al atributo Nombre de la clase Persona.
+            ApellidoP = lastname1; // Asigna el valor del parámetro lastname1 al atributo ApellidoP de la clase Persona.
+            ApellidoM = lastname2; // Asigna el valor del parámetro lastname2 al atributo ApellidoM de la clase Persona.
+            // Edad = age; // Asigna el valor del parámetro age al atributo Edad de la clase Persona.
+            Fechanacimiento = birthDate; // Asigna el valor del parámetro birthDate al atributo Fechanacimiento de la clase Persona.
+        }
+
+
+        // Las Propiedades get y set permiten acceder y modificar los valores de los atributos de la clase desde fuera de la clase.
+        // Si solo tiene get y no tiene set, la propiedad será de solo lectura y no se podrá modificar desde fuera de la clase.
+        // Si Solo tiene set y no tiene get, la propiedad será de solo escritura y no se podrá leer desde fuera de la clase.
+        // Si un atributo es privado, no se podrá acceder a él desde fuera de la clase, pero si tiene una propiedad pública con get y set, se podrá acceder y modificar el valor del atributo desde fuera de la clase.
+        public string Nombre { get; set; }
+        public string ApellidoP { get; set; }
+        public string ApellidoM { get; set; }
+        public int Edad { get; set; }
+        public DateTime Fechanacimiento { get; set; }
+    }
+}
